f0f96401430801f07ecef6b2481ded841d4cd9d7
李子晗 <homepig.lzh@qq.com>
2022-02-03 22:20:30 +0800

    Create project

---
+++ ElectricNet.Core/Area.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ElectricNet.Core
+{
+    public class Area
+    {
+        private bool[,] _area;
+
+        /// <summary>
+        /// 边界点
+        /// </summary>
+        public readonly Point Size;
+
+        /// <summary>
+        /// 生成空白区域
+        /// </summary>
+        /// <param name="size">表示尺寸的点</param>
+        public Area(Point size)
+        {
+            Size = size;
+            _area = new bool[size.X, size.Y];
+        }
+
+        /// <summary>
+        /// 用指定的数组生成区域
+        /// </summary>
+        /// <param name="origin">二维数组</param>
+        public Area(bool[,] origin)
+        {
+            Size.X = (uint)origin.GetLength(0);
+            Size.Y = (uint)origin.GetLength(1);
+            _area = (bool[,])origin.Clone();
+        }
+
+        /// <summary>
+        /// 获取某个点在区域内对应位置的值
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public bool this[Point point]
+        {
+            get { return _area[point.X, point.Y]; }
+            set { _area[point.X, point.Y] = value; }
+        }
+
+        /// <summary>
+        /// 获取在区域内某个坐标对应位置的值
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public bool this[uint X, uint Y]
+        {
+            get { return _area[X, Y]; }
+            set { _area[X, Y] = value; }
+        }
+
+        public static Area operator &(Area area1, Area area2)
+        {
+            if (area1.Size != area2.Size)
+            {
+                throw new ArgumentException("The sizes of the areas are not equal.");
+            }
+            Area area = new Area(area1.Size);
+            for(uint ix = 0; ix < area.Size.X; ix++)
+            {
+                for(uint iy = 0; iy < area.Size.Y; iy++)
+                {
+                    area[ix, iy] = (area1[ix, iy] && area2[ix, iy]);
+                }
+            }
+            return area;
+        }
+
+        public static Area operator |(Area area1, Area area2)
+        {
+            if (area1.Size != area2.Size)
+            {
+                throw new ArgumentException("The sizes of the areas are not equal.");
+            }
+            Area area = new Area(area1.Size);
+            for (uint ix = 0; ix < area.Size.X; ix++)
+            {
+                for (uint iy = 0; iy < area.Size.Y; iy++)
+                {
+                    area[ix, iy] = (area1[ix, iy] || area2[ix, iy]);
+                }
+            }
+            return area;
+        }
+
+        public static Area operator !(Area area1)
+        {
+            Area area = new Area(area1.Size);
+            for (uint ix = 0; ix < area.Size.X; ix++)
+            {
+                for (uint iy = 0; iy < area.Size.Y; iy++)
+                {
+                    area[ix, iy] = !area1[ix, iy];
+                }
+            }
+            return area;
+        }
+
+        public static Area operator -(Area area1, Area area2)
+        {
+            if (area1.Size != area2.Size)
+            {
+                throw new ArgumentException("The sizes of the areas are not equal.");
+            }
+            Area area = new Area(area1.Size);
+            for (uint ix = 0; ix < area.Size.X; ix++)
+            {
+                for (uint iy = 0; iy < area.Size.Y; iy++)
+                {
+                    area[ix, iy] = (area1[ix, iy] && !area2[ix, iy]);
+                }
+            }
+            return area;
+        }
+
+        public static Area operator +(Area area1, Area area2)
+        {
+            return (area1 | area2);
+        }
+    }
+}
---
+++ ElectricNet.Core/Point.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ElectricNet.Core
+{
+    /// <summary>
+    /// 方向
+    /// </summary>
+    public enum Direction
+    {
+        Left = -1,
+        Right = 1,
+        Up = -2,
+        Down = 2,
+    }
+
+    /// <summary>
+    /// 表示点的坐标
+    /// </summary>
+    public struct Point
+    {
+        public uint X;
+        public uint Y;
+
+        /// <summary>
+        /// 检查此点是否在允许范围内
+        /// </summary>
+        /// <param name="size">表示尺寸的点。类似于 string 的 Length 的值</param>
+        /// <returns></returns>
+        public bool IsLegal(Point size)
+        {
+            // X or Y = 2^32-1 是非法点,仅能用于表示尺寸,一定不能过 IsLegal 
+            return X < size.X && Y < size.Y;
+        }
+
+        /// <summary>
+        /// 检查此点是否在区域内
+        /// </summary>
+        /// <param name="area">区域</param>
+        /// <returns></returns>
+        public bool IsLegal(Area area)
+        {
+            return this.IsLegal(area.Size);
+        }
+
+        /// <summary>
+        /// 获取指定方向的相邻的点
+        /// </summary>
+        /// <param name="direction">方向</param>
+        /// <returns></returns>
+        public Point GetNextPoint(Direction direction)
+        {
+            unchecked
+            {
+                switch (direction)
+                {
+                    case Direction.Left:
+                        return new Point { X = this.X - 1, Y = this.Y };
+                    case Direction.Right:
+                        return new Point { X = this.X + 1, Y = this.Y };
+                    case Direction.Up:
+                        return new Point { X = this.X, Y = this.Y - 1 };
+                    case Direction.Down:
+                        return new Point { X = this.X, Y = this.Y + 1 };
+                    default:
+                        //这个没用
+                        return this;
+                }
+            }
+        }
+
+        public static bool operator ==(Point point1, Point point2)
+        {
+            return point1.X == point2.X && point1.Y == point2.Y;
+        }
+
+        public static bool operator !=(Point point1, Point point2)
+        {
+            return !(point1 == point2);
+        }
+    }
+}
